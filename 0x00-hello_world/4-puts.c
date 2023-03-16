51e5a640b58bae20b0e38fe289b21a03acda8392
Bright <you@nwobabright.com>
2023-03-16 16:37:13 -0700

    runing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - this is my main function
-*return: always 0 (success)
+* main - this is my main function
+* return: always 0 (success)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
