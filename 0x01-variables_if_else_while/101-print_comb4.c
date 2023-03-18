4842ae4b8fedbb21343d7ac1c033cb3dccddd156
Bright <you@nwobabright.com>
2023-03-18 08:16:41 -0700

    c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+ /**
+*main - Entry point
+*Discription: print comb4
+*Return: always 0
+*/
+int main(void)
+{
+	int d, p, q;
+
+	for (d = '0'; d <= '9'; d++)
+	for (p = d + 1; p <= '9'; p++)
+	for (q = p + 1; q <= '9'; q++)
+	if ((p != d) != q)
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+ /**
+*main - Entry point
+*Return: always 0
+*/
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
+	for (q = p + 1; q <= 98; q++)
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+	putchar('\n');
+	return (0);
+}
