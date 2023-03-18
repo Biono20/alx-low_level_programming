823788d20eb4e97826fff2d98497b23d435fb56b
Bright <you@nwobabright.com>
2023-03-17 20:10:01 -0700

    y

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,20 @@
 #include <stdlib.h>
-   #include <time.h>
-   #include <stdio.h>
-   /**
-   *main - Entry point
-   *Discription:
-   *Return: always 0
-   */
-   int main(void)
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0
+*/
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+	for (low = 'a'; low <= 'z'; low++)
+	if (low != e && low != q)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
 
