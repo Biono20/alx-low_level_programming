8d25d2102d9fcc9173c0c6af82c3d6376a9596ce
Bright <you@nwobabright.com>
2023-03-19 17:31:41 -0700

    c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,17 +12,18 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < '9'; i++)
 	{
-	for (j = 1; j < 10; j++)
+	for (j = i + 1; j <= '9'; j++)
 	{
-	for (k = 2; k < 10; k++)
+	for (k = j + 1; k <= 9; k++)
 	{
-	if (i < j && j < k)
+	if ((j != i) != k)
 	{
-	putchar(i + '0');
-	putchar(j + '0');
-	putchar(k + '0');
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	if (i == '7' && j == '8')
 	putchar(',');
 	putchar(' ');
 	}
