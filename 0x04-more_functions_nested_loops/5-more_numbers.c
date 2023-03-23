b72d20faebf3062809481846781f5c092d55e703
Bright <you@nwobabright.com>
2023-03-23 16:53:53 -0700

    m

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,5 +10,16 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0
-
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
