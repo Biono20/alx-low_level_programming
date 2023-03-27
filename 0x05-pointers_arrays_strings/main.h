570508161cbf73c44fc5be104be7e897782c5919
Bright <you@nwobabright.com>
2023-03-27 12:48:12 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 0x05. C - Pointers, arrays and strings
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +14,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
