1777d43e3adf520ae070364bdb687191ab10d4d8
Bright <you@nwobabright.com>
2023-04-05 14:53:49 -0700

    101-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,4 @@
-include "main.h"
-
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
-
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is, 0 it's not
- */
-int is_palindrome(char *s)
-{
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
-}
+#include "main.h"
 
 /**
  * _strlen_recursion - returns the length of a string
@@ -26,7 +10,8 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
