2f6e64309c0b565f0d9ad8e471e36a90b0abb284
Bright <you@nwobabright.com>
2023-04-05 15:14:51 -0700

    101-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,18 +15,31 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
+ * comparator - compares each character of the string
+ * @s: string
+ * @n1: smallest iterator
+ * @n2: biggest iterator
+ * Return: .
  */
-int check_pal(char *s, int i, int len)
+int comparator(char *s, int n1, int n2)
 {
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	if (i >= len)
+	if (*(s + n1) == *(s + n2))
+	{
+		if(n1 == n2 || n1 == n2 + 1)
+			return (1);
+	return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - detects if a string is a palindrome
+ * @s: string
+ * Return: 1 if s is a palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
